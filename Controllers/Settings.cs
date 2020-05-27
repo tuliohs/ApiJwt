f066200ebb9552bdd7bf6aff707fb8c623c8c4eb
tuliohs <tuliohhenriquepaz@gmail.com>
2020-05-26 22:24:11 -0300

    FirtExecution

--- Controllers/Settings.cs
+++
@@ -1,7 +0,0 @@
-namespace ApiJwt
-{
-    public static class Settings
-    {
-        public static string Secret = "fedaf7d8863b48e197sadfadsf4564asdfasd57b9287d492b768a4sd08e";
-    }
-}
---
+++ Services/TokenServices.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using ApiJwt.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ApiJwt.TokenService
+{
+    public static class TokenService
+    {
+        public static string GenerarToken(User user)
+        {
+            //JwtSecurityTokenHandler - metodo que gera o token
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // Econdando (transformando para bytes)
+            var key = Encoding.ASCII.GetBytes(Settings.SecretKeyPrivate);
+
+            //Descrição do Token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                //Subject - Clains que ficam disponivel - por enquanto serão: name, role
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Username.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role.ToString())
+                }),
+
+                //Expires - quando o token expira
+                Expires = DateTime.UtcNow.AddHours(2),
+
+                //SigningCredentials - parametros: SymmetricSecurityKey é a chave que será passada,  e o HmacSha256Signature é o algoritmos hash para encriptação
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            //CreateToken - Metodo que cria o token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            //WriteToke : leitura de um token e conversão para string
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
