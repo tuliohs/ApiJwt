d02a6ad63cc1a9d39a56280e4010551b3b5942cc
tuliohs <tuliohhenriquepaz@gmail.com>
2020-05-26 22:11:16 -0300

    Initial

---
+++ Controllers/Settings.cs
@@ -0,0 +1,7 @@
+namespace ApiJwt
+{
+    public static class Settings
+    {
+        public static string Secret = "fedaf7d8863b48e197sadfadsf4564asdfasd57b9287d492b768a4sd08e";
+    }
+}
---
+++ Properties/Models.User.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiJwt.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
